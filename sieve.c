cef896ed4b989ee2198ce9740a79a1e27c6d82b6
trickfinger <jacques.simoes21@gmail.com>
2015-06-03 13:52:10 -0300

    Update sieve.c

--- sieve.c
+++ sieve.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
 #include <math.h>
-
 #define MAX 1000000
 
 int main()
 {
-    unsigned int crivo[MAX];
-    double num;
-    int i, j, result;
-	
-        for(i=0;i<MAX;i++)
-            scanf("%lf", num);
 
-        crivo[MAX] = 0;
-        aux = sqrt(num);
-            aux = floor(aux);
+	unsigned int crivo[MAX];
+	double num;
+	int i, j, result;
 
-        for(i=2;i<MAX;i++) {
+		scanf("%lf", &num);
+	for(i=0;i<num;i++)
+		
+	crivo[MAX] = 0;
+		aux = floor(sqrt(num);
 
-            if(crivo[MAX] == 0)
-                    crivo[MAX] = 1;
+	for(i=2;i<aux;i++) {
+		if(crivo[MAX] == 0)
+	crivo[MAX] = 1;
 
-                for(j=i+i;j<MAX;j = j + i)
-                            crivo[MAX] = 2;                        
-        }
-                    result = 0;
-                    for(i=2;i<MAX;i++)
-                        
-                        if(crivo[MAX] == 1) 
-                                result = result + i;
+	for(j=i+i;j<MAX;j = j + i)
+	crivo[MAX] = 2;
+}
+	result = 0;
 
-                                    printf("%d", result);
+	for(i=2;i<MAX;i++)
+		if(crivo[MAX] == 1)
+	result = result + i;
+		printf("%d", result);
 
-	return (0);
+return (0);
 }
